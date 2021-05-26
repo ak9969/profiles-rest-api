f7a10e0482b24ad29747da7026b485d7e4fed257
ak9969 <jakshat100@gmail.com>
2021-05-26 23:48:09 +0530

    Updated post patch delete

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -14,9 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-
+from django.urls import path
+from profiles_api import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/',include('profiles_api.urls'))
+    path('hello-view/', views.HelloApiView.as_view()),
 ]
